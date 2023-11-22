b761ef0d8ee825a3e73332a2943feaaf66694f04
Ryan Shoff <ryanshoff@gmail.com>
2023-11-22 12:33:44 -0600

    updated vers and testing navx

--- hook.c
+++ hook.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define __USE_GNU
 #include <dlfcn.h>
 
-int HAL_CreateSimDevice(const char* name) {
+int32_t HAL_CreateSimDevice(const char* name) {
 	printf("HAL_CreateSimDevice: %s\n", name);
-	if(!strncmp(name, "SPARK", 5)) {  // pretend we are not in a simulation for REV
+	if(!strncmp(name, "SPARK", 5) || !strncmp(name, "navX", 4)) {  
+		// pretend we are not in a simulation 
 		printf("skipping orig HAL_CreateSimDevice\n");
 		return 0;
 	} else {  			// otherwise act normal
@@ -28,4 +32,54 @@ int HAL_CreateSimDevice(const char* name) {
 			return origFunc(name);
 		}
 	}
+}
+
+
+/*
+HAL_ENUM(HAL_SerialPort) {
+  HAL_SerialPort_Onboard = 0,
+  HAL_SerialPort_MXP = 1,
+  HAL_SerialPort_USB = 2,
+  HAL_SerialPort_USB1 = 2,
+  HAL_SerialPort_USB2 = 3
+};
+*/
+
+int serial_port = 0;
+
+int32_t HAL_InitializeSerialPort(int32_t port,
+                                              int32_t* status) {
+	printf("HAL_InitializeSerialPort: %d\n", port);
+	serial_port = open("/dev/ttyACM0", O_RDWR);
+
+	return 0;
+}
+
+int32_t HAL_InitializeSerialPortDirect(int32_t port,
+                                                    const char* portName,
+                                                    int32_t* status) {
+	printf("HAL_InitializeSerialPortDirect: %s %d\n", portName, port);
+	serial_port = open("/dev/ttyACM0", O_RDWR);
+
+	return 0;
+}
+
+int32_t HAL_ReadSerial(int32_t handle, char* buffer, int32_t count,
+                       int32_t* status) {
+	printf("HAL_ReadSerial\n");
+	int32_t n = read(serial_port, buffer, count);
+  	return n;
+}
+
+int32_t HAL_WriteSerial(int32_t handle, const char* buffer,
+                        int32_t count, int32_t* status) {
+	printf("HAL_WriteSerial\n");
+	int32_t n = write(serial_port, buffer, count);
+  	return n;
+}
+
+void HAL_CloseSerial(int32_t handle, int32_t* status) {
+	printf("HAL_CloseSerial\n");
+	close(serial_port);
+	serial_port = 0;
 }
